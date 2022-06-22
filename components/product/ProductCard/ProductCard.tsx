048c5d88ee65452a1ca75bffecb3d68c754932d7
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2022-06-23 00:09:14 +0600

    working product styling

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { ProductType } from "@common/types/product";
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
+import s from "./ProductCard.module.css";
 
 interface ProductCardProps {
   product: ProductType;
@@ -10,15 +11,17 @@ const placeholder = "/assets/product-image-placeholder.svg";
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
     <Link href={`/product/${product?.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={s.root}>
+        <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>14 $ </span>
+          <span className={s.productPrice}>14 $ </span>
         </div>
         {product?.images && (
           <Image
+            className={s.productImage}
             width={540}
             height={540}
             src={product?.images[0]?.url ?? placeholder}
